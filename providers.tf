084b60a972c38abb7e1dd76320efded26a4154ec
tsingh <tsingh@atpco.net>
2024-10-15 16:54:26 -0700

    Tried using the oidc syntax to authenticate to azure

--- providers.tf
+++ providers.tf
@@ -4,5 +4,14 @@ provider "azurerm" {
     subscription_id = var.azure_subscription_id
     client_id       = var.azure_client_id
     tenant_id       = var.azure_tenant_id
-    use_oidc        = true
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "Site2Site_rg_development"
+    storage_account_name  = "tsblobstore11development"
+    container_name        = "terraform-state"
+    key                   = "Site2Site_VPC_development.tfstate"
+    use_oidc              = true
+  }
 }
