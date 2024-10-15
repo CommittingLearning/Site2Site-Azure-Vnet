b2a073aae21e325c9da7fd22d36533c3e713167f
tsingh <tsingh@atpco.net>
2024-10-15 16:13:03 -0700

    using backend commands in the workflows script to not expose creds

--- providers.tf
+++ providers.tf
@@ -6,16 +6,3 @@ provider "azurerm" {
     tenant_id       = var.azure_tenant_id
     use_oidc        = true
 }
-
-terraform {
-  backend "azurerm" {
-    storage_account_name   = "tsblobstore11development"
-    container_name         = "terraform-state"
-    key                    = "Site2Site_VPC_development.tfstate"
-    resource_group_name    = "Site2Site_rg_development"
-    use_oidc               = true
-    client_id              = var.azure_client_id
-    tenant_id              = var.azure_tenant_id
-    subscription_id        = var.azure_subscription_id
-  }
-}
