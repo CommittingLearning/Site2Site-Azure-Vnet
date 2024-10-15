76fe0d5bc978dd74c9c1b37f0a3e86e76f1feb7a
tsingh <tsingh@atpco.net>
2024-10-15 16:07:56 -0700

    added authentication mechanism for backend

--- providers.tf
+++ providers.tf
@@ -13,5 +13,9 @@ terraform {
     container_name         = "terraform-state"
     key                    = "Site2Site_VPC_development.tfstate"
     resource_group_name    = "Site2Site_rg_development"
+    use_oidc               = true
+    client_id              = var.azure_client_id
+    tenant_id              = var.azure_tenant_id
+    subscription_id        = var.azure_subscription_id
   }
 }
